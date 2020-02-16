dac740644b8584065b73c3a81c5f1f0d0b484a27
MarianaCouto <61013815+MarianaCouto@users.noreply.github.com>
2020-02-16 19:27:33 -0300

    Final aula11

--- Aula11/Aula11.cs
+++ Aula11/Aula11.cs
@@ -1,11 +1,23 @@
 using System;
 
-class Aula11{
+class Aula11{  // type cast conversão de um tipo em outro
     static void Main(){
+
+        int n1=10;
+        float n2=n1;
+        float n3=10.5f;
+        int n4=(int)n3;  //isto é uma operação de type cast
+        short n5=(short)n1;
+
+
+        Console.WriteLine(n2);
+        Console.WriteLine(n4);
 
 
 
     }
     
+
+
 }
 
