406967eacaa4ca7b1edf32aec1ad1bf1b74fd8c2
MarianaCouto <61013815+MarianaCouto@users.noreply.github.com>
2020-02-16 18:46:36 -0300

    Final aula09

--- Aula09/Aula09.cs
+++ Aula09/Aula09.cs
@@ -1,9 +1,18 @@
 using System;
 
-class AulaModelo{
-    static void Main(){
+class Aula09{  //Operadores bitWise
+     static void Main(){
+
+         int num = 10;
+         int num2 = 10;
+
+         num=num<<1;  // para esquerda dobra
+         num2=num2>>1; // para direita metade
 
 
+         Console.WriteLine(num);
+         Console.WriteLine(num2);
+         
 
     }
     
