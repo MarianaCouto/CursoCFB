934ae2e858dba7a835c3fa46e60762c2f4a878e4
MarianaCouto <61013815+MarianaCouto@users.noreply.github.com>
2020-02-16 19:04:48 -0300

    Final Aula10

--- Aula10/Aula10.cs
+++ Aula10/Aula10.cs
@@ -1,7 +1,15 @@
 using System;
 
-class Aula10{
-    static void Main(){
+class Aula10{  // Enum ou Enumeradores
+
+    enum DiasSemana{Domingo,Segunda,Terça,Quarta,Quinta,Sexta,Sábado};
+
+        static void Main(){
+            
+            DiasSemana ds = (DiasSemana)3;    // ou assim DiasSemana ds = DiasSemana.Domingo;
+            int ds1 = (int) DiasSemana.Sexta;  // ou descobrir o indice de um valor
+            Console.WriteLine(ds);
+            Console.WriteLine(ds1);
 
 
 
